ac50282579c79f03d0ad3590ba6c4b939df1e830
Jonah <c0025745@my.shu.ac.uk>
2023-01-31 15:43:46 +0000

    Update contactUs.php

--- project/contactUs.php
+++ project/contactUs.php
@@ -1,14 +1,14 @@
-<link rel ="stylesheet" type="text/css" href="style.css"/>
+
 <html>
   <head>
     <title>Contact Us</title>
   </head>
 
   <body>
-    <?php echo '<p>Contact Us</p>'; ?>
-    <?php echo '<p>To Access For All</p>'; ?>
-    <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-    <?php echo '<p>From';?>
+    <?php echo '<p>Contact Us</p>'; 
+    echo '<p>To: Access For All</p>'; ?>
+    <textarea name="comment" rows="10" cols="80"></textarea>
+    <?php echo '<p>From:';?>
     <input type="text">
 
     <h4>(+44) 7492 043842 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; AccessForAll@gmail.com</h4>
