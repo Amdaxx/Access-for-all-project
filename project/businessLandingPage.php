2c1e90f828518321b642452a3ddf0129634b3d5b
DavidAni44 <82607483+DavidAni44@users.noreply.github.com>
2023-02-09 12:30:51 +0000

    modded landing businessLandingPage

--- project/businessLandingPage.php
+++ project/businessLandingPage.php
@@ -9,10 +9,9 @@
         <img src="Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img>
         <li style="float:right"><a class="active" href="landingPage.php">Sign Out</a></li>
       </ul>    
-    </div>
+</div>
 
     <div id="s-container">
         <div class="Brec">test</div>
         <div class="Srec1"></div>
-
     </div>
