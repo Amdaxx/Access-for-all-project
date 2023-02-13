611bc399706701feff6c8c3b848c34dec3c2b10f
xclwright <xclwright@gmail.com>
2023-02-13 19:41:59 +0000

    Update businessLandingPage.php

--- Everbody-Welcome/business/businessLandingPage.php
+++ Everbody-Welcome/business/businessLandingPage.php
@@ -3,20 +3,20 @@
 <link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavhome.css">
 
 
- <div id="header">
-      <ul>
-        <li style = "font-size: 40px" >Welcome: </li>
-          <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img>
-        <li style="float:right"><a class="active" href="http://localhost/Access-for-all-project/Everbody-Welcome/public/landingPage.php">Sign Out</a></li>
-      </ul>    
+<div id="header">
+  <ul>
+    <li style = "font-size: 40px" >Company Name Here</li>
+    <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img>
+    <li style="float:right"><a class="active" href="http://localhost/Access-for-all-project/Everbody-Welcome/public/landingPage.php">Sign Out</a></li>
+  </ul>    
 </div>
 
-      <div class="Srec1" ><br><br>
-        <h1 id="viewAudit"><a href="#">View Audits</a></h1>
-      </div>
+<div class="Srec1" ><br><br>
+  <h1 id="viewAudit"><a href="#">View Audits</a></h1>
+</div>
 
-      <div class="Srec2" ><br><br>
-        <h1 id="newAudit"><a href="#">New Audits</a></h1>
-      </div>
+<div class="Srec2" ><br><br>
+  <h1 id="newAudit"><a href="#">New Audits</a></h1>
+</div>
 
       
