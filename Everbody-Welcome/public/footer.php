f83c4c53a3fa4ffa23a9c0b420090ec897593337
xclwright <xclwright@gmail.com>
2023-02-13 13:26:51 +0000

    footer

--- Everbody-Welcome/public/footer.php
+++ Everbody-Welcome/public/footer.php
@@ -1,14 +1,34 @@
-<link rel="stylesheet" href="site.css" />
-    <footer  class="border-top footer text-muted">
-            <div class="container">
-                &copy; january 2023 - Access for all
-            </div>
-    </footer>
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/footer.css"/>
+<footer>
+    <div class="row">
+      <div class="fourcolumn">
+        <div class="card">
+          <h3>Search For Venue</h3>
+        </div>
+      </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
+      <div class="fourcolumn">
+        <div class="card">
+          <h3>Our Services</h3>
+        </div>
+      </div>
+      
+      <div class="fourcolumn">
+        <div class="card">
+          <h3>Contact Us</h3>
+        </div>
+      </div>
+
+      <div class="fourcolumn">
+        <div class="card">
+          <h3>Contact Us</h3>
+          <button1 onclick="window.location.href='landingPage.php';" type= "button" ><img src="pictures\Everybody-Welcome-logo.png" alt="contact us icon" width="225" height="225"></img></button1>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row" style = "text-align:left; margin-left:15px">
+     © Copywright - Everybody Welcome           
+    </div>
+</footer>
