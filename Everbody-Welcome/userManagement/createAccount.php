73a827dd94ec4fcf9879c217a250b9890771deee
xclwright <xclwright@gmail.com>
2023-02-14 11:49:35 +0000

    working

--- Everbody-Welcome/userManagement/createAccount.php
+++ Everbody-Welcome/userManagement/createAccount.php
@@ -8,22 +8,15 @@ if (isset($_POST['submit'])){
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/style.css">  
-<link rel="stylesheet" href="vvvvvvv">
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavhome.css">
 
-<header>
-    <div id="header">
-      <ul>
-        <li><a href="http://localhost/Access-for-all-project/Everbody-Welcome/userManagement/userLogin.php">Log In</a></li>
-        <button2 onclick="window.location.href='http://localhost/Access-for-all-project/Everbody-Welcome/public/landingPage.php';" type= "button" ><img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img></button2>
-        <li style="float:right"><a class="active" href="http://localhost/Access-for-all-project/Everbody-Welcome/userManagement/createAccount.php">Create Account</a></li>
-      </ul>    
-    </div>
-  </header>
-
+<div id="header">
+  <?php include "../public/publicHeader.php" ?>     
+</div>
 <body>
     <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="logo" class="logo">
-<form id = "centerform" method="post" >
-<div class="form-group row">
+    <form id = "centerform" method="post" >
+    <div class="form-group row">
     <label for="cName" class="col-sm-2 col-form-label">Company Name</label>
     <div class="col-sm-6">
       <input type="text" class="form-control" id="cName" placeholder="Company Name" name="companyName"> <br>
--- Everbody-Welcome/userManagement/userLogIn.php
+++ Everbody-Welcome/userManagement/userLogIn.php
@@ -13,13 +13,9 @@ if (isset($_POST['submit'])) {
 <link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavhome.css">
 
 
-    <div id="header">
-      <ul>
-        <li><a href="http://localhost/Access-for-all-project/Everbody-Welcome/userManagement/userLogin.php">Log In</a></li>
-        <button2 onclick="window.location.href='http://localhost/Access-for-all-project/Everbody-Welcome/public/landingPage.php';" type= "button" ><img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img></button2>
-        <li style="float:right"><a class="active" href="http://localhost/Access-for-all-project/Everbody-Welcome/userManagement/createAccount.php">Create Account</a></li>
-      </ul>    
-    </div>
+<div id="header">
+    <?php include "../public/publicHeader.php" ?>     
+</div>
   <body>
     <br><br><br>
     <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="logo" class="logo">
