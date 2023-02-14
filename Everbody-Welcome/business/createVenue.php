6c562cb1c8d6dbde814147dcd4dcf21bc7f73856
Jonah <c0025745@my.shu.ac.uk>
2023-02-14 10:45:57 +0000

    Create venue
    updated create venue

--- Everbody-Welcome/business/createVenue.php
+++ Everbody-Welcome/business/createVenue.php
@@ -17,33 +17,39 @@
     </div>
 
     <div class="seventyfive">
-      <h3>
+      
 
-      </h3>
-
-      <h3><input type="text" placeholder="Enter venue name"></h3>
-      <h3><input type="text" placeholder="Enter venue address"></h3>
-      <h3><input type="text" placeholder="Enter venue postcode"></h3>
-
-      <h3><label for="typeOfVenue">Type of Venue</label>
-        <select name="typeOfVenue" id="typeOfVenue">
-            <option value="Accommodation">Accommodation</option>
-            <option value="Airbnb">Airbnb</option>
-            <option value="Experience">Experience</option>
-            <option value="Tours">Tours</option>
-            <option value="Events">Events</option>
-      </select>
-      </h3>
-
-
-
-
-        <div class = "twocolumn" >
-          <div class="box">
-          </div>
+    <form id = "centerform" >
+    <div class="form-group row">
+        <label for="vName" class="col-sm-2 col-form-label">Venue name</label>
+        <div class="col-sm-6">
+        <input type="text" class="form-control" id="vName" placeholder="Venue Name"> <br>
         </div>
-
+    </div>
+    <div class="form-group row">
+        <label for="vAddress" class="col-sm-2 col-form-label">Venue address</label>
+        <div class="col-sm-6">
+        <input type="text" class="form-control" id="vAddress" placeholder="Venue Address"> <br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="vPostcode" class="col-sm-2 col-form-label">Postcode</label>
+        <div class="col-sm-6">
+        <input type="text" class="form-control" id="vPostcode" placeholder="Venue Postcode"> <br>
+        </div>
     </div>
 
+    <div class="form-group row">
+        <label for="typeOfVenue" class="col-sm-2 col-form-label">Type of Venue</label>
+        <div class="col-sm-6">
+            <select name="typeOfVenue" id="typeOfVenue">
+                <option value="Accommodation">Accommodation</option>
+                <option value="Airbnb">Airbnb</option>
+                <option value="Experience">Experience</option>
+                <option value="Tours">Tours</option>
+                <option value="Events">Events</option>
+            </select>
+        </div>
+    </div>
   </body>
 </html>
