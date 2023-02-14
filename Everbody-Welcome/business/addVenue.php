8729bf293824b69c3f28344ab12310b028b09075
Jonah <c0025745@my.shu.ac.uk>
2023-02-14 10:33:56 +0000

    Create and add venue pages
    basic setup

---
+++ Everbody-Welcome/business/addVenue.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/style.css">  
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavHome.css">  
+<html>
+  <head>
+    <title>Add Venue</title>
+  </head>
+
+  <body>
+
+    <div class="row">
+      <div class = "onecolumn">
+        <aboutUs>
+          <h2 >Add a Venue</h2>
+        </aboutUs>
+      </div>
+    </div>
+
+    <div class="seventyfive">
+      <h3>
+
+      </h3>
+
+      <h3><input type="text" placeholder="Enter venue name"></h3>
+      <h3><input type="text" placeholder="Enter venue address"></h3>
+      <h3><input type="text" placeholder="Enter venue postcode"></h3>
+
+      <h3><label for="typeOfVenue">Type of Venue</label>
+        <select name="typeOfVenue" id="typeOfVenue">
+            <option value="Accommodation">Accommodation</option>
+            <option value="Airbnb">Airbnb</option>
+            <option value="Experience">Experience</option>
+            <option value="Tours">Tours</option>
+            <option value="Events">Events</option>
+      </select>
+      </h3>
+
+
+
+
+        <div class = "twocolumn" >
+          <div class="box">
+          </div>
+        </div>
+
+    </div>
+
+  </body>
+</html>
---
+++ Everbody-Welcome/business/createOrAddVenue.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/style.css">  
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavHome.css">  
+<html>
+  <head>
+    <title>Create or Add Venue</title>
+  </head>
+
+  <body>
+    
+    
+    <div class="row">
+        <div class="card">
+          <h3><b>Create or Add a Venue</b></h3>
+          <button onclick="window.location.href='createVenue.php';" type= "button" id="createVenue" class="button">Create Venue</button>
+          <label for="createVenue"></label>
+        </div>
+        <div class="card">
+          <button onclick="window.location.href='addVenue.php';" type= "button" id="addVenue" class="button">Add Venue</button>
+          <label for="addVenue"></label>
+        </div>
+    </div>
+   
+
+  </body>
+</html>
