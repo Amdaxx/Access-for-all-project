7605349c886b610a23b6945e474a6f1efd1411ef
xclwright <xclwright@gmail.com>
2023-02-14 11:32:14 +0000

    changes

--- Everbody-Welcome/business/businessLandingPage.php
+++ Everbody-Welcome/business/businessLandingPage.php
@@ -14,9 +14,8 @@ $id = $_SESSION['id'];
 
 ?>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/style.css">  
-<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavhome.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/businessHomepage.css">
 
 <body style = "background-color:cdc7c7">
   <div id="header">
