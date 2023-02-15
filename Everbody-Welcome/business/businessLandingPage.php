37f3d7d3f51a8c12f7ab785489652e01fc9b4494
xclwright <xclwright@gmail.com>
2023-02-15 23:54:47 +0000

    Reactive bbusiness landing page

--- Everbody-Welcome/business/businessLandingPage.php
+++ Everbody-Welcome/business/businessLandingPage.php
@@ -8,35 +8,37 @@
   <div id="header">
     <?php include "businessHeader.php";
      $res = displayBusinessDetails($_SESSION['id']);
-     
-    ?>    
-  </div>
-
-  <div class="twocolumn">
-    <div class="button-background">
-      <btn-group>
-        <button><a href="survey.php">New Audit</a></button>
-        <button><a href="survey.php">Edit Audit</a></button>
-        <button><a href="updateBusinessDetails.php">Edit Company Information</a></button>
-      </btn-group>
-    </div>
+    ?>  
   </div>
 
   <div class = "row">
     <div class="twocolumn">
-      <h1 class = "counter-text">Company Information</h1>
-      <p class="text"><u>Company Name: </u> </p>
-      <?php echo $res[0]; ?>
-      <p class="text"><u>Email: </u></p>
-      <?php  echo $res[4]?>
-      <p class="text"><u>Address: </u></p>
-      <?php echo $res[1]; ?>
-      <p class="text"><u>Post Code: </u></p>
-      <?php echo $res[2]; ?>
-      <p class="text"><u>Contact Number: </u></p>
-      <?php echo $res[3]; ?>
-      <p class="text"><u>Venues Added: </u></p>       
-      <?php echo $res[5]; ?>
+      <div class="button-background">
+        <btn-group>
+          <button><a href="viewVenues.php">View Venues</a></button>
+          <button><a href="createVenue.php">Create New Venue</a></button>
+          <button><a href="updateBusinessDetails.php">Edit Company Information</a></button>
+        </btn-group>
+      </div>
+    </div>
+
+  
+    <div class="twocolumn" >
+      <div class ="company-info">
+        <h1><b>Company Information</b></h1>
+        <p><u>Company Name: </u> </p>
+        <?php echo $res[0]; ?>
+        <p><u>Email: </u></p>
+        <?php  echo $res[4]?>
+        <p><u>Address: </u></p>
+        <?php echo $res[1]; ?>
+        <p><u>Post Code: </u></p>
+        <?php echo $res[2]; ?>
+        <p><u>Contact Number: </u></p>
+        <?php echo $res[3]; ?>
+        <p><u>Venues Added: </u></p>       
+        <?php echo $res[5]; ?>
+      </div> 
     </div> 
   </div>
 
