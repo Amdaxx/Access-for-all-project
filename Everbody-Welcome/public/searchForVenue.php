7bfdf6c652a99221cf45ab2687502c5c09f2f386
ConnerBowen <93921961+ConnerBowen@users.noreply.github.com>
2023-02-15 12:33:42 +0000

    Update searchForVenue.php

--- Everbody-Welcome/public/searchForVenue.php
+++ Everbody-Welcome/public/searchForVenue.php
@@ -1,6 +1,76 @@
 <html>
   <head>
-  
+  <style>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  background-color: #696969;
+  padding: 10px;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+/* Sidebar links */
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+
+/* Active/current link */
+.sidebar a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Links on mouse-over */
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+/* On screens that are less than 700px wide, make the sidebar into a topbar */
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+
+</style>
     <title> Everybody Welcome</title>
     <link rel="stylesheet" href="http://localhost/Access-for-all-project/Everbody-Welcome/css/topNavHome.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
@@ -24,28 +94,40 @@
     <form action="/action_page.php">
       <input type="text" placeholder="Search.." name="search"style="width: 600px; margin-left:25% ">
       <button type="submit">Submit</button>
-    
-    <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+        </div>
+    <div class="sidebar">
   <h2 class="w3-bar-item">Filter Search</h2>
   <h4 class="w3-bar-item">Venue Type:</h4>
   <input type="checkbox" id="vType" name="VenueType" value="vType">
   <label for="vehicle1"> Accommodation</label><br>
-</div>
+
 <input type="checkbox" id="vType" name="VenueType" value="vType">
   <label for="vehicle1"> Experience</label><br>
-</div>
+
 <input type="checkbox" id="vType" name="VenueType" value="vType">
   <label for="vehicle1"> Tours</label><br>
-</div>
+
 <input type="checkbox" id="vType" name="VenueType" value="vType">
   <label for="vehicle1"> Events</label><br>
-</div>
+
 
  <input type="text" class="form-control" id="pstcode" name="PostCode" placeholder="PostCode" style="width: 100px"> <br>
- </div>
+        </div>
 <div style="margin-left:25%">
-
-
+<div class="button-background">
+   
+  </div>
+  <div class="grid-container">
+  <div class="grid-item"><img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"><br>Everyone Welcome</div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>  
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>  
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item">9</div>  
+</div>
 
   </body>
 
