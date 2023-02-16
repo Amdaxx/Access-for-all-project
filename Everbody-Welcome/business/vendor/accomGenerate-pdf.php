48c1867e4b70f601dcbeb53f514fb6ebd76f6cfe
Jonah <c0025745@my.shu.ac.uk>
2023-02-16 10:06:13 +0000

    Update accomGenerate-pdf.php

--- Everbody-Welcome/business/vendor/accomGenerate-pdf.php
+++ Everbody-Welcome/business/vendor/accomGenerate-pdf.php
@@ -94,167 +94,166 @@ $a22 = $_POST["accomAnswerTwentyTwo"];
 $commentTwentyTwo = $_POST["accomComment22"];
 
 
-$html = '<h1>Accomodation Survey Results</h1>'; //title
-$html .= "
-<head>
-<style>
-body {background-color: rgb(168,168,168);}
-h1   {color:black;}
-p    {color: red;}
-</style>
-</head>
+$html = "<head>
+        <style>
+        body {background-color: rgb(168,168,168);}
+        h1   {color:black;}
+        p    {color: red;}
+        </style>
+        <h1>Accomodation Survey Results</h1>
+        </head>
 
-<body background-color: >
-<table border='1' style='border-collapse:collapse'>
-            <tr>
-                <th>Questions</th>
-                <th>Yes/No</th>
-                <th>Proof</th>
-                <th>Comments</th>
-            </tr>
-            <tr>
-                <td width='250'>Are clearly marked designated accessible parking spaces available at or near the venue?</td>
-                <td width='60'>$a1</td>
-                <td width='250'></td>
-                <td width='200'>$commentOne</td>
-            </tr>
-            <tr>
-                <td width='250'>Does the main entrance have level access (without steps or raised thresholds)? Level access includes access by ramp (permanent or temporary) or lift.</td>
-                <td width='60'>$a2</td>
-                <td width='250'></td>
-                <td width='200'>$commentTwo</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there an alternative entrance, with level access (without steps or raised thresholds)? Level access includes access by ramp (permanent or temporary) or lift.</td>
-                <td width='60'>$a3</td>
-                <td width='250'></td>
-                <td width='200'>$commentThree</td>
-            </tr>
-            <tr>
-                <td width='250'>Is the clear door opening width of the entrance with level access more than 75cm?</td>
-                <td width='60'>$a4</td>
-                <td width='250'></td>
-                <td width='200'>$commentFour</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there level access (without steps or thresholds) from the accessible entrance to the public toilet, including access by ramp or lift and the door opening is at least 75cm wide?</td>
-                <td width='60'>$a5</td>
-                <td width='250'></td>
-                <td width='200'>$commentFive</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there space for a wheelchair to at least one side of the toilet to enable the person to transfer?</td>
-                <td width='60'>$a6</td>
-                <td width='250'></td>
-                <td width='200'>$commentSix</td>
-            </tr>
-            <tr>
-                <td width='250'>Are there handrails next to the toilet?</td>
-                <td width='60'>$a7</td>
-                <td width='250'></td>
-                <td width='200'>$commentSeven</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there an emergency call system in the toilet?</td>
-                <td width='60'>$a8</td>
-                <td width='250'></td>
-                <td width='200'>$commentEight</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bedroom: Is there level access (without steps or thresholds) from the accessible entrance to the bedroom, including access by ramp or lift
-                AND the route from the accessible entrance to the bedroom is at least 75cm wide, at it’s narrowest point 
-                AND the door opening is at least 75cm wide
-                AND the bedroom has unobstructed floor space not less than 120cm by 90cm?</td>
-                <td width='60'>$a9</td>
-                <td width='250'></td>
-                <td width='200'>$commentNine</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bathroom: Is the door opening at least 75cm wide AND
-                is there unobstructed floor space not less than 120cm by 90cm?</td>
-                <td width='60'>$a10</td>
-                <td width='250'></td>
-                <td width='200'>$commentTen</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bathroom: Is there space for a wheelchair to at least one side of the toilet to enable the person to transfer?</td>
-                <td width='60'>$a11</td>
-                <td width='250'></td>
-                <td width='200'>$commentEleven</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bathroom: Are there handrails next to the toilet?</td>
-                <td width='60'>$a12</td>
-                <td width='250'></td>
-                <td width='200'>$commentTwelve</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bathroom: Is there a level access wetroom shower (no raised shower tray or step-up)?</td>
-                <td width='60'>$a13</td>
-                <td width='250'></td>
-                <td width='200'>$commentThirteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Wheelchair accessible bathroom: Is there an emergency call system in the bathroom?</td>
-                <td width='60'>$a14</td>
-                <td width='250'></td>
-                <td width='200'>$commentFourteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there a hearing loop/audio induction loop/infrared system available to amplify sound
-                AND is there clear signage positioned where any fixed loops are effective?</td>
-                <td width='60'>$a15</td>
-                <td width='250'></td>
-                <td width='200'>$commentFifteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there a mobile hoist available or a ceiling tracked hoist in at least one accessible bedroom?</td>
-                <td width='60'>$a16</td>
-                <td width='250'></td>
-                <td width='200'>$commentSixteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there an emergency call system with vibrating pillow pad available to alert someone with hearing loss if the fire alarm is activated?</td>
-                <td width='60'>$a17</td>
-                <td width='250'></td>
-                <td width='200'>$commentSeventeen</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there an emergency call system with flashing lights to alert someone with hearing loss if the fire alarm is activated?</td>
-                <td width='60'>$a18</td>
-                <td width='250'></td>
-                <td width='200'>$commentEighteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Is there is level access (without steps or thresholds) from the accessible entrance to the restaurant, including access by ramp or lift
-                AND is the route from the accessible entrance to the restaurant at least 75cm wide, at it’s narrowest point 
-                AND is the door opening at least 75cm wide?</td>
-                <td width='60'>$a19</td>
-                <td width='250'></td>
-                <td width='200'>$commentNineteen</td>
-            </tr>
-            <tr>
-                <td width='250'>Can you can cater for a range of dietary requirements such as vegan, gluten free, lactose free?</td>
-                <td width='60'>$a20</td>
-                <td width='250'></td>
-                <td width='200'>$commentTwenty</td>
-            </tr>
-            <tr>
-                <td width='250'>Are trained assistance dogs welcome?</td>
-                <td width='60'>$a21</td>
-                <td width='250'></td>
-                <td width='200'>$commentTwentyOne</td>
-            </tr>
-            <tr>
-                <td width='250'>Do you have a published Accessibility Guide - a thorough yet concise description of the accessibility of your venue/experience? If so, please provide a link in the comments section.</td>
-                <td width='60'>$a22</td>
-                <td width='250'></td>
-                <td width='200'>$commentTwentyTwo</td>
-            </tr>
+        <body background-color: grey>
+        <table border='1' style='border-collapse:collapse'>
+                    <tr>
+                        <th>Questions</th>
+                        <th>Yes/No</th>
+                        <th>Proof</th>
+                        <th>Comments</th>
+                    </tr>
+                    <tr>
+                        <td width='250'>Are clearly marked designated accessible parking spaces available at or near the venue?</td>
+                        <td width='60'>$a1</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentOne</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Does the main entrance have level access (without steps or raised thresholds)? Level access includes access by ramp (permanent or temporary) or lift.</td>
+                        <td width='60'>$a2</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTwo</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there an alternative entrance, with level access (without steps or raised thresholds)? Level access includes access by ramp (permanent or temporary) or lift.</td>
+                        <td width='60'>$a3</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentThree</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is the clear door opening width of the entrance with level access more than 75cm?</td>
+                        <td width='60'>$a4</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentFour</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there level access (without steps or thresholds) from the accessible entrance to the public toilet, including access by ramp or lift and the door opening is at least 75cm wide?</td>
+                        <td width='60'>$a5</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentFive</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there space for a wheelchair to at least one side of the toilet to enable the person to transfer?</td>
+                        <td width='60'>$a6</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentSix</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Are there handrails next to the toilet?</td>
+                        <td width='60'>$a7</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentSeven</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there an emergency call system in the toilet?</td>
+                        <td width='60'>$a8</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentEight</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bedroom: Is there level access (without steps or thresholds) from the accessible entrance to the bedroom, including access by ramp or lift
+                        AND the route from the accessible entrance to the bedroom is at least 75cm wide, at it’s narrowest point 
+                        AND the door opening is at least 75cm wide
+                        AND the bedroom has unobstructed floor space not less than 120cm by 90cm?</td>
+                        <td width='60'>$a9</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentNine</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bathroom: Is the door opening at least 75cm wide AND
+                        is there unobstructed floor space not less than 120cm by 90cm?</td>
+                        <td width='60'>$a10</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bathroom: Is there space for a wheelchair to at least one side of the toilet to enable the person to transfer?</td>
+                        <td width='60'>$a11</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentEleven</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bathroom: Are there handrails next to the toilet?</td>
+                        <td width='60'>$a12</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTwelve</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bathroom: Is there a level access wetroom shower (no raised shower tray or step-up)?</td>
+                        <td width='60'>$a13</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentThirteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Wheelchair accessible bathroom: Is there an emergency call system in the bathroom?</td>
+                        <td width='60'>$a14</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentFourteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there a hearing loop/audio induction loop/infrared system available to amplify sound
+                        AND is there clear signage positioned where any fixed loops are effective?</td>
+                        <td width='60'>$a15</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentFifteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there a mobile hoist available or a ceiling tracked hoist in at least one accessible bedroom?</td>
+                        <td width='60'>$a16</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentSixteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there an emergency call system with vibrating pillow pad available to alert someone with hearing loss if the fire alarm is activated?</td>
+                        <td width='60'>$a17</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentSeventeen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there an emergency call system with flashing lights to alert someone with hearing loss if the fire alarm is activated?</td>
+                        <td width='60'>$a18</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentEighteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Is there is level access (without steps or thresholds) from the accessible entrance to the restaurant, including access by ramp or lift
+                        AND is the route from the accessible entrance to the restaurant at least 75cm wide, at it’s narrowest point 
+                        AND is the door opening at least 75cm wide?</td>
+                        <td width='60'>$a19</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentNineteen</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Can you can cater for a range of dietary requirements such as vegan, gluten free, lactose free?</td>
+                        <td width='60'>$a20</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTwenty</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Are trained assistance dogs welcome?</td>
+                        <td width='60'>$a21</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTwentyOne</td>
+                    </tr>
+                    <tr>
+                        <td width='250'>Do you have a published Accessibility Guide - a thorough yet concise description of the accessibility of your venue/experience? If so, please provide a link in the comments section.</td>
+                        <td width='60'>$a22</td>
+                        <td width='250'></td>
+                        <td width='200'>$commentTwentyTwo</td>
+                    </tr>
 
-        </table>
-</body>
-"; //body
+                </table>
+        </body>
+        "; //body
 
 $options = new Options;
 $options->setChroot(__DIR__);
