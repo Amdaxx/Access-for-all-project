260434518298049882e9be485ac00e92055e4a57
xclwright <xclwright@gmail.com>
2023-02-16 12:50:33 +0000

    SORTED FORM

--- Everbody-Welcome/business/updateBusinessDetails.php
+++ Everbody-Welcome/business/updateBusinessDetails.php
@@ -48,28 +48,6 @@ function validateForm() {
 <body>
     <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="logo" class="logo">
 <form id = "centerform" method="post" onsubmit="return validateForm()">
-<div class="form-group row">
-    <label for="cName" class="col-sm-2 col-form-label">Company Name </label>
-    <div class="col-sm-6">
-      <input type="text" name="cname" class="form-control" id="cName" placeholder="<?php echo $res[0];?>"> <br>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="mail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-6">
-      <input type="email" name = "mail" class="form-control" id="mail" placeholder="<?php echo $res[4];?>"> <br>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="addr" class="col-sm-2 col-form-label">Address</label>
-    <div class="col-sm-6">
-      <input type="text" name="address" class="form-control" id="addr" placeholder="<?php echo $res[2];?>"> <br>
-    </div>
-  </div>
-
-  <body>
-      <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="logo" class="logo">
-  <form id = "centerform" method="post">
   <div class="form-group row">
       <label for="cName" class="col-sm-2 col-form-label">Company Name </label>
       <div class="col-sm-6">
