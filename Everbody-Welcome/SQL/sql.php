317bc0384638a9edafb19a0a25ac2ad1a885585c
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-02-21 20:26:33 +0000

    addQuestion to DB

--- Everbody-Welcome/SQL/sql.php
+++ Everbody-Welcome/SQL/sql.php
@@ -39,8 +39,7 @@
                 )";
               
                 $sql4 = "CREATE TABLE questions(
-                    question VARCHAR(50) NOT NULL,
-                    type VARCHAR(15) NOT NULL
+                    question VARCHAR(50) NOT NULL
                     )";
                 mysqli_query($conn, $sql1);
                 mysqli_query($conn, $sql2);   
@@ -220,18 +219,18 @@
         }   
     }
 
-    function addQuestion($question, $type)
+    function addQuestion($question)
     {
         try{
             $conn = connectToDatabase();
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $conn->prepare("INSERT INTO questions (question, type) 
-            VALUES (:question, :type)");
-            $stmt->bindParam(':question', $question);
-            $stmt->bindParam(':type', $type);
-            $stmt->execute();           
+            $conn->beginTransaction();
+            $conn->exec("INSERT INTO questions (question)
+                VALUES ('question1')");   
+            $conn->exec("INSERT INTO questions (question)
+            VALUES ('question2')");            
             
+            $conn->commit();
         } catch(PDOException $e) {
             echo "Error: " . $e->getMessage();
         } 
@@ -345,3 +344,5 @@
         $stmt->execute();
         header('Location:  ../business/viewVenues.php?id='.$id);
     }
+
+ 
