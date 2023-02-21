34f99b1b88162b51e2b5008b5eaa238ecdc6b2d8
xclwright <xclwright@gmail.com>
2023-02-21 22:45:50 +0000

    responsive image

--- Everbody-Welcome/userManagement/userLogIn.php
+++ Everbody-Welcome/userManagement/userLogIn.php
@@ -30,37 +30,37 @@ if (isset($_POST['submit'])) {
   <body>
     <br><br><br>
     <img src="http://localhost/Access-for-all-project/Everbody-Welcome/pictures/Everybody-Welcome-logo.png" alt="logo" class="logo">
-        <form id = "centerform" method="post" onsubmit="return validateForm()">
-        <div class="form-group row">
-            <label for="mail" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-6">
-            <input type="email" class="form-control" id="mail" name="email" placeholder="Email"> <br>
-            </div>
+    <form id = "centerform" method="post" onsubmit="return validateForm()">
+    <div class="form-group row">
+        <label for="mail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-6">
+        <input type="email" class="form-control" id="mail" name="email" placeholder="Email"> <br>
         </div>
-        <div class="form-group row">
-            <label for="pwd" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-6">
-            <input type="password" class="form-control" id="pwd" name="pass" placeholder="Password"> <br>
-            <input type="checkbox" onclick="myFunction()">Show Password
+    </div>
+    <div class="form-group row">
+        <label for="pwd" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-6">
+        <input type="password" class="form-control" id="pwd" name="pass" placeholder="Password"> <br>
+        <input type="checkbox" onclick="myFunction()">Show Password
 
-            <script>
-        function myFunction() {
-        var x = document.getElementById("pwd");
-        if (x.type === "password") {
-            x.type = "text";
-        } else {
-            x.type = "password";
-        }
-        }
-        </script>
-            </div>
+        <script>
+    function myFunction() {
+    var x = document.getElementById("pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+    }
+    </script>
         </div>
-        <div class="form-group row">
-            <div class="col-sm-10">
-            <button type="submit" name="submit" class="btn btn-primary">Log in</button>
-            </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-10">
+        <button type="submit" name="submit" class="btn btn-primary">Log in</button>
         </div>
-        </form>
+    </div>
+    </form>
         
     </body>
     <?php include  '../public/footer.php' ?>
