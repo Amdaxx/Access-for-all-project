f808b5b969e6eb07c457a1ba8912756f84b4933a
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-07 22:35:52 +0000

    change

--- Everbody-Welcome/public/testpage.php
+++ Everbody-Welcome/public/testpage.php
@@ -2,5 +2,16 @@
 require_once('../SQL/sql.php');
 
 echo "hehe";
-$conn = connectToDatabase();
-?>
+
+$servername = 'afpproject-server.mysql.database.azure.com';
+$username = 'tkgwwyrhag';
+$password = 'RI41G4S77ZWA1QYW';
+$dbname = 'afpproject-database';
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}?>
