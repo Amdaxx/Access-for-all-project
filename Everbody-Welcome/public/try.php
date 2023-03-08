cf2d5b11e1093007ed009092eec521b9efb11d35
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-08 10:34:00 +0000

    change

---
+++ Everbody-Welcome/public/try.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "hello"
+?>
+<br>
+
+<?php
+
+$servername = "afpproject-server.mysql.database.azure.com";
+$username = "adels";
+$password = "adels";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
