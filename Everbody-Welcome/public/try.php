d97562130c487ba51019aece3f039509a367eab1
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-08 19:47:32 +0000

    Update try.php

--- Everbody-Welcome/public/try.php
+++ Everbody-Welcome/public/try.php
@@ -14,5 +14,9 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+else
+{
+    echo "c'est good kho";
+}
 
 ?>
