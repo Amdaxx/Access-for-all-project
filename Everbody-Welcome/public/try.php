95ce35f3ced1a17f4204632cb2a08f569b634c89
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-08 19:54:22 +0000

    Update try.php

--- Everbody-Welcome/public/try.php
+++ Everbody-Welcome/public/try.php
@@ -2,17 +2,17 @@
 
 <?php
 
-define("DB_SERVER", "afpproject-server.mysql.database.azure.com" );
-define("DB_USERNAME", "tkgwwyrhag");
-define("DB_PASSWORD", "Blackdown12345");
+
 define("DB_NAME", "afadb");
  
+echo "conn 1:";
+
 /* Attempt to connect to MySQL database */
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = new mysqli("afpproject-server.mysql.database.azure.com", "tkgwwyrhag", "Blackdown12345", "afadb");
  
 // Check connection
 if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
+    echo "c'est pas bon du tt";
 }
 else
 {
@@ -20,3 +20,22 @@ else
 }
 
 ?>
+
+<br>
+<br>
+<br>
+
+<?php
+echo "conn 2:";
+
+/* Attempt to connect to MySQL database */
+$mysqli = new mysqli("afpproject-server.mysql.database.azure.com", "tkgwwyrhag", "Blackdown12345", "afpproject-database");
+ 
+// Check connection
+if($mysqli === false){
+    echo "c'est pas bon du tt";
+}
+else
+{
+    echo "c'est good kho";
+}
