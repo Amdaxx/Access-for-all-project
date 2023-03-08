d281c485b1978719f9333baa21374ba3d72cf843
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-08 19:15:48 +0000

    Update try.php

--- Everbody-Welcome/public/try.php
+++ Everbody-Welcome/public/try.php
@@ -6,17 +6,22 @@ echo "hello"
 
 <?php
 
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "../public/DigiCertGlobalRootCA.crt", NULL, NULL);
+if (!$conn) {
+    die("mysqli_init failed");
+    echo "A";
+}
 
- $conn = mysqli_init();
- mysqli_ssl_set($conn,NULL,NULL, "../public/DigiCertGlobalRootCA.crt", NULL, NULL);
- if(mysqli_real_connect($conn, "afpproject-server.mysql.database.azure.com", "tkgwwyrhag", "Blackdown12345", "afpproject-database", 3306, MYSQLI_CLIENT_SSL)==TRUE)
- {
-    echo "oui";
- }
- else
- {
-    echo "non";
- }
+if (!mysqli_real_connect($conn, "afpproject-server.mysql.database.azure.com", "tkgwwyrhag", "Blackdown12345", "afpproject-database", 3306, MYSQLI_CLIENT_SSL)) {
+    die("Connect Error: " . mysqli_connect_error());
+    echo "B";
+}
+
+if ($conn) {
+    echo "Connection successful!";
+}
+
 
 
 ?>
