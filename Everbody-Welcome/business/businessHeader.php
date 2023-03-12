e23887369026df35b8685c3389b03cce2749e325
David Ani <davidarisani@gmail.com>
2023-03-12 13:10:15 +0000

    responsive header

--- Everbody-Welcome/business/businessHeader.php
+++ Everbody-Welcome/business/businessHeader.php
@@ -18,15 +18,15 @@ $id = $_SESSION['id'];
 
 <link rel="stylesheet" href="../css/style.css">  
 <link rel="stylesheet" href="../css/topNavhome.css">
-<link rel="stylesheet" href="../css/overall-page.css">
+<link rel="stylesheet" href="../css/businessHeader.css">
 
 <div id="header">
   <ul>
     <button3 onclick="window.location.href='../business/businessLandingPage.php';" type= "button" >
-      <li><img src="../pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></img></li>
-      <li style = "font-size: 40px" ><?php echo $_SESSION['name']?></li>
+      <li><img src="../pictures/Everybody-Welcome-logo.png" alt="Logo" width="100" height="50"></li>
+      <li style="font-size: 40px;"><?php echo $_SESSION['name']?></li>
     </button3>
-    <li style="float:right"><a class="active" href="../public/landingPage.php">Sign Out</a></li>
+    <li class="sign-out"><a class="active" href="../public/landingPage.php">Sign Out</a></li>
   </ul>    
 </div>
 
