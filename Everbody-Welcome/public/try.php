ba478af4ba5db566551a17513bf35c6c88f69eca
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-13 14:55:31 +0000

    Update try.php

--- Everbody-Welcome/public/try.php
+++ Everbody-Welcome/public/try.php
@@ -21,7 +21,7 @@ if ($mysqli->connect_errno) {
 }
 
 
-$sql3 = "CREATE TABLE venues(
+$sql = "CREATE TABLE venues(
     venueid VARCHAR(10) NOT NULL PRIMARY KEY ,
     logid VARCHAR(10) NOT NULL,
     venuename VARCHAR(30) NOT NULL,
@@ -30,32 +30,12 @@ $sql3 = "CREATE TABLE venues(
     type VARCHAR(15) NOT NULL ,
     numberofaudits NOT NULL
 )";
-$sql4 = "CREATE TABLE questions(
-    question VARCHAR(50) NOT NULL
-    )";
-$sql5 = "CREATE TABLE audits(
-    venueid VARCHAR(10) NOT NULL ,
-    question VARCHAR(50) NOT NULL ,
-    answer VARCHAR(3) NOT NULL ,
-    comment VARCHAR(40) ,
-    proof VARCHAR(30) ,
-    auditnumber INTEGER NOT NULL 
-)";
 
-  if ($mysqli->query($sql3) === TRUE) {
+  if ($mysqli->query($sql) === TRUE) {
     echo "Table venues created successfully";
   } else {
     echo "Error creating table: " . $mysqli->error;
   }
-  if ($mysqli->query($sql4) === TRUE) {
-    echo "Table questions created successfully";
-  } else {
-    echo "Error creating table: " . $mysqli->error;
-  }
-  if ($mysqli->query($sql5) === TRUE) {
-    echo "Table audits created successfully";
-  } else {
-    echo "Error creating table: " . $mysqli->error;
-  }
+
 ?>
 
