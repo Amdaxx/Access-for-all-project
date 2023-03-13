11e95282460750dfd22aef7e15ee11d404f6f2b4
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-13 13:52:45 +0000

    Update try.php

--- Everbody-Welcome/public/try.php
+++ Everbody-Welcome/public/try.php
@@ -20,20 +20,7 @@ if ($mysqli->connect_errno) {
     echo "Connected successfully!";
 }
 
-/*
-$sql1 = "CREATE TABLE logs(
-    logid VARCHAR(10) NOT NULL PRIMARY KEY ,
-    email VARCHAR(30) NOT NULL,
-    pwd VARCHAR(300) NOT NULL,
-    stat VARCHAR(30) NOT NULL 
-)";
-$sql2 = "CREATE TABLE businessinfos(
-    logid VARCHAR(10) NOT NULL PRIMARY KEY ,
-    companyName VARCHAR(30) NOT NULL,
-    postcode VARCHAR(10) NOT NULL,
-    address VARCHAR(30) NOT NULL,
-    phone VARCHAR(15) NOT NULL
-)";
+
 $sql3 = "CREATE TABLE venues(
     venueid VARCHAR(10) NOT NULL PRIMARY KEY ,
     logid VARCHAR(10) NOT NULL,
@@ -54,16 +41,7 @@ $sql5 = "CREATE TABLE audits(
     proof VARCHAR(30) ,
     auditnumber INTEGER NOT NULL 
 )";
-if ($mysqli->query($sql1) === TRUE) {
-    echo "Table logs created successfully";
-  } else {
-    echo "Error creating table: " . $mysqli->error;
-  }
-  if ($mysqli->query($sql2) === TRUE) {
-    echo "Table businessinfo created successfully";
-  } else {
-    echo "Error creating table: " . $mysqli->error;
-  }
+
   if ($mysqli->query($sql3) === TRUE) {
     echo "Table venues created successfully";
   } else {
@@ -80,4 +58,4 @@ if ($mysqli->query($sql1) === TRUE) {
     echo "Error creating table: " . $mysqli->error;
   }
 ?>
-*/
+
