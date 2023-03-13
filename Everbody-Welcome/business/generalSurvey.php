8f98a43db1991784d29f8073708b04fa6ed83df2
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-13 18:08:50 +0000

    changes

--- Everbody-Welcome/business/generalSurvey.php
+++ Everbody-Welcome/business/generalSurvey.php
@@ -1,3 +1,22 @@
+<?php
+include_once("../SQL/sql.php");
+$path = "../public/LandingPage.php";
+
+
+session_start();
+
+if (!isset($_SESSION['business'])){
+    session_unset();
+    session_destroy();
+    header("Location:".$path);
+}
+checkSession ($path); //calling the function from session.php
+
+$id = $_SESSION['id']; 
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
