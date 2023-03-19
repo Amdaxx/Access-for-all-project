1705e07252cb5a94c086a03d5298615754b199fd
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-19 13:54:25 +0000

    changes

---
+++ Everbody-Welcome/business/qrcode.php
@@ -0,0 +1,17 @@
+<?php
+
+// Include the QR code library
+include "phpqrcode/qrlib.php";
+
+// The URL you want to encode into the QR code
+$url = "https://afpproject.azurewebsites.net/Everbody-Welcome/public/landingPage.php";
+
+
+// Set the size and error correction level of the QR code
+$size = 10;
+$level = "L";
+
+// Generate the QR code and output it to the browser
+QRcode::png($url, false, $level, $size);
+
+?>
