0fce7fbfee2d15c76844fddcbc9538ae8661cafb
Jonah <c0025745@my.shu.ac.uk>
2023-03-21 14:10:17 +0000

    Update userLogIn.php

--- Everbody-Welcome/userManagement/userLogIn.php
+++ Everbody-Welcome/userManagement/userLogIn.php
@@ -23,12 +23,23 @@ if (isset($_POST['submit'])){
         <h2>Login Form</h2>
         <form method="post">
           <label for="email">Email</label>
-          <input type="email" id="email" required />
+          <input type="email" name="email" required />
 
           <label for="password">Password</label>
-          <input type="password" id="password" required />
+          <input type="password" name="pwd" required />
 
           <button type="submit">Login</button>
+
+          <script>
+            const form = document.querySelector('.login-form');
+            form.addEventListener('submit', function(event) {
+              event.preventDefault();
+              const email = form.email.value;
+              const password = form.password.value;
+              // Add your login logic here
+            });
+          </script>
+
         </form> 
         <p>Don't have an account? <a href="createAccount.php">Sign up</a></p>
       </div>
