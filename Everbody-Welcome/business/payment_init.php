37b84d5577f8a54ca76c9fdd290873bc8e714d0e
DavidAni44 <82607483+DavidAni44@users.noreply.github.com>
2023-03-22 20:48:29 +0000

    test fix

--- Everbody-Welcome/business/payment_init.php
+++ Everbody-Welcome/business/payment_init.php
@@ -5,6 +5,8 @@ require_once 'config.php';
  
 // Include the Stripe PHP library 
 require_once '../stripe-php-10.9.0-beta.1/init.php';
+
+$path='.:/usr/local/lib/php';
  
 // Set API key 
 $stripe = new \Stripe\StripeClient(STRIPE_API_KEY);
