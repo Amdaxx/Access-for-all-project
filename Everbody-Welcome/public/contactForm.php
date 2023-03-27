e2a75505469ed05c7adb8279d95596c4a65aec88
Jonah <c0025745@my.shu.ac.uk>
2023-03-27 19:13:06 +0100

    Update contactForm.php

--- Everbody-Welcome/public/contactForm.php
+++ Everbody-Welcome/public/contactForm.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit'])){
     $comment = $_POST['comment'];
     $email = $_POST['email'];
 
-    $mailTo = "c0025745@hallam.shu.ac.uk";
+    $mailTo = "jonahcole01091989@gmail.com";
     $headers = "From: ".$email;
     $txt = "You have received an email from Everybody Welcome.\n\n".$comment;
 
