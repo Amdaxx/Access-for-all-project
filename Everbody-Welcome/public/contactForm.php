b0b084a948f17efc1ef9054bbcbedc1ca7a4882d
Jonah <c0025745@my.shu.ac.uk>
2023-03-27 19:19:34 +0100

    Update contactForm.php

--- Everbody-Welcome/public/contactForm.php
+++ Everbody-Welcome/public/contactForm.php
@@ -1,13 +1,17 @@
-<?php 
-if(isset($_POST['submit'])){
+<?php
+if (isset($_POST['submit'])) {
     $comment = $_POST['comment'];
     $email = $_POST['email'];
+    $headers = 'From: '.$email."\r\n".
+        'Reply-To: '.$email."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    $subject = 'Message from Everybody Welcome Website';
+    $to = 'jonahcole01091989@gmail.com';
+    $message = "You have received an email from Everybody Welcome. \n\n" .
+               "Message: " . $comment . "\n\n" .
+               "From: " . $email;
 
-    $mailTo = "jonahcole01091989@gmail.com";
-    $headers = "From: ".$email;
-    $txt = "You have received an email from Everybody Welcome.\n\n".$comment;
-
-    mail($mailTo, $comment, $txt, $headers);
+    mail($to, $subject, $message, $headers);
     header("Location: landingPage.php?mailsent");
 }
 ?>
