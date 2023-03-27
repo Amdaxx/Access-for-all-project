6b8d2d7a1c7f6e6c76cb2ac8fd9948d9ccb50bbe
Jonah <c0025745@my.shu.ac.uk>
2023-03-27 18:50:15 +0100

    Create contactForm.php

---
+++ Everbody-Welcome/public/contactForm.php
@@ -0,0 +1,13 @@
+<?php 
+
+if($_POST['submit']){
+    $comment = $_POST['comment'];
+    $email = $_POST['email'];
+
+    $mailTo = "c0025745@hallam.shu.ac.uk";
+    $headers = "From: ".$email;
+    $txt = "You have received an email from Everybody Welcome.\n\n".$comment;
+
+    mail($mailTo, $comment, $txt, $headers);
+    header("Location: landingPage.php?mailsent");
+}
