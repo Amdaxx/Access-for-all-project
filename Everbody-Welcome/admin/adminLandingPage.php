3f89a462fbbcca922f26f809a0f5c5b609902b0a
Jonah <c0025745@my.shu.ac.uk>
2023-03-27 11:51:02 +0100

    Update adminLandingPage.php

--- Everbody-Welcome/admin/adminLandingPage.php
+++ Everbody-Welcome/admin/adminLandingPage.php
@@ -14,3 +14,59 @@ checkSession ($path); //calling the function from session.php
 
 $id = $_SESSION['id']; 
 ?>
+
+<head>
+<title>Business Landing Page</title>
+<meta name="description" content="This page displays information about the user's account such as company name and email. Also,
+on this page there are links to the venues pages (view venues and create new venue) as well as a link to edit company information.">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="../css/businessHomepage.css">
+<link rel="stylesheet" href="../css/overall-page.css">
+</head>
+
+<div class="flex-wrapper">
+  <div id="header">
+      <?php include "businessHeader.php";
+      $res = displayBusinessDetails($_SESSION['id']);
+      ?>  
+  </div>
+
+  <body style = "background-color:cdc7c7">
+    <div class = "row">
+      <div class="twocolumn">
+        <div class="button-background">
+          <btn-group>
+            <button><a href="viewVenues.php">View Venues</a></button>
+            <button><a href="createVenue.php">Create New Venue</a></button>
+            <button><a href="updateBusinessDetails.php">Edit Company Information</a></button>
+          </btn-group>
+        </div>
+      </div>
+
+      
+      <div class="twocolumn" >
+        <div class ="company-info">
+          <h1><b>Company Information</b></h1>
+          <p><u>Company Name: </u> </p>
+          <?php echo $res[0]; ?>
+          <p><u>Email: </u></p>
+          <?php  echo $res[4]?>
+          <p><u>Address: </u></p>
+          <?php echo $res[2]; ?>
+          <p><u>Post Code: </u></p>
+          <?php echo $res[1]; ?>
+          <p><u>Contact Number: </u></p>
+          <?php echo $res[3]; ?>
+          <p><u>Venues Added: </u></p>       
+          <?php echo $res[5]; ?>
+        </div> 
+      </div> 
+    </div> 
+  </div>
+
+  
+
+  </body>
+  <?php include "../public/footer.php" ?>
+  
+</div>  
