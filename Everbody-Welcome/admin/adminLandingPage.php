837ae684c71579c1d5cafdfa89a552adec2fb472
Jonah <c0025745@my.shu.ac.uk>
2023-03-27 11:55:57 +0100

    Update adminLandingPage.php

--- Everbody-Welcome/admin/adminLandingPage.php
+++ Everbody-Welcome/admin/adminLandingPage.php
@@ -47,17 +47,17 @@ $id = $_SESSION['id'];
         <div class ="company-info">
           <h1><b>Company Information</b></h1>
           <p><u>Company Name: </u> </p>
-          <?php echo $res[0]; ?>
+          
           <p><u>Email: </u></p>
-          <?php  echo $res[4]?>
+          
           <p><u>Address: </u></p>
-          <?php echo $res[2]; ?>
+          
           <p><u>Post Code: </u></p>
-          <?php echo $res[1]; ?>
+          
           <p><u>Contact Number: </u></p>
-          <?php echo $res[3]; ?>
+          
           <p><u>Venues Added: </u></p>       
-          <?php echo $res[5]; ?>
+          
         </div> 
       </div> 
     </div> 
