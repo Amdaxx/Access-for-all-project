be5f3614f4bb88c6c7500e87948522e87f598eef
Jonah <c0025745@my.shu.ac.uk>
2023-03-28 18:52:33 +0100

    Update venuePage.php

--- Everbody-Welcome/public/venuePage.php
+++ Everbody-Welcome/public/venuePage.php
@@ -1,47 +1,54 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="../css/style.css">  
-<link rel="stylesheet" href="../css/topNavHome.css">  
-<link rel="stylesheet" href="../css/overall-page.css">
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="This page displays the venue information including venue name, venue type and the venue's
-    answers to the general survey.">
+    <meta name="description" content="This page displays the venue information including venue name, venue type and the venue's answers to the general survey.">
     <title>Venue Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">  
+    <link rel="stylesheet" href="../css/topNavHome.css">  
+    <link rel="stylesheet" href="../css/overall-page.css">
   </head>
   <body>
-    <div class="flex-wrapper">
-      <?php include "publicHeader.php" ?>  
+    <?php include "publicHeader.php" ?>
+    <div class="container">
       <div class="text-center">
-        <font size="+2">
-          <h2>Venue Name: Venue Type</h2>
-          <img src="../pictures/worldIcon.png" alt="world icon" width="225" height="225"></img>
-        </font>
+        <h2>Venue Name: Venue Type</h2>
+        <img src="../pictures/worldIcon.png" alt="world icon" width="225" height="225">
       </div>
 
       <div class="row">
-        <div class="col-sm-12 col-md-10 col-md-offset-1">
+        <div class="col-md-10 col-md-offset-1">
           <div class="col-xs-12">
-            <p style="font-size: 1.5em;"><strong>Question 1:</strong> What is the venue's capacity?</p>
-            <p style="font-size: 1.2em;"><strong>Answer:</strong> The venue can hold up to 500 people.</p>
             <hr>
-            <p style="font-size: 1.5em;"><strong>Question 2:</strong> Are there parking facilities?</p>
-            <p style="font-size: 1.2em;"><strong>Answer:</strong> Yes, there is a parking lot available for guests.</p>
+            <p><strong>Question 1:</strong> What is the venue's capacity?</p>
+            <p>The venue can hold up to 500 people.</p>
             <hr>
-            <p style="font-size: 1.5em;"><strong>Question 3:</strong> Is the venue accessible for people with disabilities?</p>
-            <p style="font-size: 1.2em;"><strong>Answer:</strong> Yes, the venue is wheelchair accessible.</p>
+            <p><strong>Question 2:</strong> Are there parking facilities?</p>
+            <p>Yes, there is a parking lot available for guests.</p>
             <hr>
-            <p style="font-size: 1.5em;"><strong>Question 4:</strong> What is the venue's location?</p>
-            <p style="font-size: 1.2em;"><strong>Answer:</strong> The venue is located in downtown, close to the subway station.</p>
+            <p><strong>Question 3:</strong> Is the venue accessible for people with disabilities?</p>
+            <p>Yes, the venue is wheelchair accessible.</p>
             <hr>
-            <p style="font-size: 1.5em;"><strong>Question 5:</strong> Does the venue provide catering services?</p>
-            <p style="font-size: 1.2em;"><strong>Answer:</strong> Yes, the venue has its own catering service that can provide food and drinks.</p>
+            <p><strong>Question 4:</strong> What is the venue's location?</p>
+            <p>The venue is located in downtown, close to the subway station.</p>
+            <hr>
+            <p><strong>Question 5:</strong> Does the venue provide catering services?</p>
+            <p>Yes, the venue has its own catering service that can provide food and drinks.</p>
           </div>
         </div>
       </div>
-      <hr>
-      <button class="btn btn-primary btn-lg" style="position: fixed; bottom: 20px; right: 20px;" onclick="window.location.href='../public/contactVenue.php';">Contact This Venue</button>
-      <?php include "footer.php" ?>
+
+      <div class="text-center">
+        <button class="btn btn-primary" onclick="window.location.href='../public/contactVenue.php';">Contact This Venue</button>
+      </div>
+
+      
     </div> 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </body>
+  <?php include "footer.php" ?>
 </html>
