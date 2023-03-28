ead95be80b65d0eb987e53bd79ed7ca6b30b8d09
David Ani <davidarisani@gmail.com>
2023-03-28 15:07:05 +0100

    hopefully last update

--- Everbody-Welcome/business/businessLandingPage.php
+++ Everbody-Welcome/business/businessLandingPage.php
@@ -37,8 +37,8 @@ on this page there are links to the venues pages (view venues and create new ven
   <body style = "background-color:cdc7c7">
 
   <div class = "homeButtons"> 
-    <button class="btn btn-primary btn-lg"><a href="createVenue.php">Add Venues</a></button>
-    <button class="btn btn-primary btn-lg"><a href="updateBusinessDetails.php">Edit Company Info</a></button>
+    <button class="btn btn-primary btn-md"><a href="createVenue.php">Add Venues</a></button>
+    <button class="btn btn-primary btn-md"><a href="updateBusinessDetails.php">Edit Company Info</a></button>
   </div>
       
       <?php
