48f8f286d58f8c05c8cd6fbc32a6c6ff999a439e
David Ani <davidarisani@gmail.com>
2023-03-30 21:37:18 +0100

    Update payment-cancel.php

--- Everbody-Welcome/business/payment-cancel.php
+++ Everbody-Welcome/business/payment-cancel.php
@@ -8,6 +8,17 @@
 
 <!-- Uncomment when added -->
 <!--<link href="css/style.css" rel="stylesheet"> -->
+<header id="nav-wrapper">
+    <nav id="nav">
+      <div class="nav left">
+        <span class="gradient skew"><h1 class="logo un-skew"><img src="../pictures/logo.png"></h1></span>
+        <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
+      </div>
+    </nav>
+</header>
+
+
+
 </head>
 <body>
 <div class="container">
@@ -17,4 +28,17 @@
     <a href="index.php" class="btn-link">Back to Product Page</a>
 </div>
 </body>
+
+<style>
+    #nav-wrapper {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1000; /* optional - this determines the stacking order of the element */
+}
+
+
+</style>
 </html>
+<?php include "../public/footer.php" ?>
