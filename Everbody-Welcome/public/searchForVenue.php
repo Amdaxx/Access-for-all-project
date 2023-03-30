2cd75c237cca8302a9337ddfdcc068e893d0a5e2
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-30 10:59:22 +0100

    .

--- Everbody-Welcome/public/searchForVenue.php
+++ Everbody-Welcome/public/searchForVenue.php
@@ -1,5 +1,15 @@
 <?php
 require_once('../SQL/sql.php');
+
+$res = displayAllVenues();
+
+if (isset($_POST['category']) || isset($_POST['city'])) {
+  $selected_category = $_POST['category'];
+  $selected_city = $_POST['city'];
+  $res = filterVenues($selected_category, $selected_city);
+}
+
+
 ?>
 <html>
   <head>
@@ -14,50 +24,71 @@ require_once('../SQL/sql.php');
 
         <div class="sidebar">
           <h2 class="sidebar__title">Venues (354)</h2>
+          <form action="process_form.php" method="post" id="form">
+  <div class="sidebar__category">
+    <div class="sidebar__heading">Type <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
+      <polyline points="18 15 12 9 6 15"></polyline>
+    </svg></div>
+    <div class="sidebar__options">
+      <label class="check">
+        <input type="radio" class="check__input" name="category" value="accommodation">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Accomodation</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="category" value="tours">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Tours</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="category" value="experience">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Experience</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="category" value="events">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Events</p>
+      </label>
+    </div>
+  </div>
+</form>
 
-          <div class="sidebar__category">
-            <div class="sidebar__heading">Type <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
-              <polyline points="18 15 12 9 6 15"></polyline>
-              </svg></div>
-            <div class="sidebar__options">
-              <label class="check">
-                <input type="checkbox" class="check__input">
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Accomodation</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input">
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Tours</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input">
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Experience</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input">
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Events</p>
-              </label>
-            </div>
-          </div>
+<script>
+  $(document).ready(function() {
+    $('input[type=radio]').change(function() {
+      var category = $(this).val();
+
+      $.ajax({
+        url: "process_form.php",
+        type: "post",
+        data: {category: category},
+        success: function(response) {
+          // process the response from the server
+        }
+      });
+    });
+  });
+</script>
+
+
+
 
           <div class="sidebar__category">
             <div class="sidebar__heading">Location<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
@@ -125,7 +156,6 @@ require_once('../SQL/sql.php');
         </div>
 
 
-          <?php $res = displayAllVenues(); ?>
           <div class="items">
             <?php foreach ($res as $venue): ?>
               <div class="item">
