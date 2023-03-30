b0fb4132d25229fff48e9d4ca039c33cfba726d2
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-30 11:32:59 +0100

    ,.

--- Everbody-Welcome/public/searchForVenue.php
+++ Everbody-Welcome/public/searchForVenue.php
@@ -3,7 +3,7 @@ require_once('../SQL/sql.php');
 
 $res = displayAllVenues();
 
-if (isset($_POST['category']) || isset($_POST['city'])) {
+if (isset($_POST['category'])) {
   $selected_category = $_POST['category'];
   $selected_city = $_POST['city'];
   $res = filterVenues($selected_category, $selected_city);
@@ -24,7 +24,7 @@ if (isset($_POST['category']) || isset($_POST['city'])) {
 
         <div class="sidebar">
           <h2 class="sidebar__title">Venues (354)</h2>
-          <form action="process_form.php" method="post" id="form">
+          <form  method="post" id="form">
   <div class="sidebar__category">
     <div class="sidebar__heading">Type <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
       <polyline points="18 15 12 9 6 15"></polyline>
@@ -68,82 +68,55 @@ if (isset($_POST['category']) || isset($_POST['city'])) {
       </label>
     </div>
   </div>
+
+
+
+
+  <div class="sidebar__category">
+    <div class="sidebar__heading">Location <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
+      <polyline points="18 15 12 9 6 15"></polyline>
+    </svg></div>
+    <div class="sidebar__options">
+      <label class="check">
+        <input type="radio" class="check__input" name="city" value="Sheffield">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Sheffield</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="city" value="London">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">London</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="city" value="Manchester">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Manchester</p>
+      </label>
+      <label class="check">
+        <input type="radio" class="check__input" name="city" value="Leeds">
+        <span class="check__checkbox">
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </span>
+        <p class="check__text">Leeds</p>
+      </label>
+    </div>
+  </div>
+  <button class="button" value=""filter name="filter">Search</button>
 </form>
-
-<script>
-  $(document).ready(function() {
-    $('input[type=radio]').change(function() {
-      var category = $(this).val();
-
-      $.ajax({
-        url: "process_form.php",
-        type: "post",
-        data: {category: category},
-        success: function(response) {
-          // process the response from the server
-        }
-      });
-    });
-  });
-</script>
-
-
-
-
-          <div class="sidebar__category">
-            <div class="sidebar__heading">Location<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up">
-              <polyline points="18 15 12 9 6 15"></polyline>
-              </svg></div>
-            <div class="sidebar__options">
-              <label class="check">
-                <input type="checkbox" class="check__input" checked>
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Birmingham</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input" checked>
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Manchester</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input" checked>
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Newcastle</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input" checked>
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">Liverpool</p>
-              </label>
-              <label class="check">
-                <input type="checkbox" class="check__input" checked>
-                <span class="check__checkbox">
-                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 6.5L9 17.5L4 12.5" stroke="#fff" stroke-width="2.6" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </span>
-                <p class="check__text">London</p>
-              </label>
-            </div>
-          </div>
-        </div>
-        
 
         <div class="main">
           <h2 class="main__title">Plants (354)</h2>
