719e636f34cc90208773754d926b675f595a5931
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-31 12:44:01 +0100

    .

--- Everbody-Welcome/userManagement/createAccount.php
+++ Everbody-Welcome/userManagement/createAccount.php
@@ -1,22 +1,27 @@
 <link rel="stylesheet" href="../css/userManagementStyle.css">
-<?php include '../public/publicHeader.php';?>
+<?php include '../public/publicHeader.php';
+
+if (isset($_POST['submit'])){
+    createUser($_POST['email'], $_POST['pwd'], $_POST['name'], $_POST['phone'], $_POST['postcode'], $_POST['address'],);
+    }
+    ?>
 <body>
     <div class="signup-container">
     <div class="signup-form">
         <h2>Sign Up</h2>
         <form method="post">
         <label for="companyname">Company Name</label>
-        <input type="text" id="companyname" placeholder="Enter your company name" required>
+        <input type="text" name="name" id="companyname" placeholder="Enter your company name" required>
         <label for="email">Email</label>
-        <input type="email" id="email" placeholder="Enter your email address" required>
+        <input type="email" name="email" id="email" placeholder="Enter your email address" required>
         <label for="address">Address</label>
-        <input type="text" id="address" placeholder="Enter your address" required>
+        <input type="text" name="address" id="address" placeholder="Enter your address" required>
         <label for="postcode">Postcode</label>
-        <input type="text" id="postcode" placeholder="Enter your postcode" required>
+        <input type="text" name="postcode" id="postcode" placeholder="Enter your postcode" required>
         <label for="phone">Phone</label>
-        <input type="text" id="phone" placeholder="Enter your phone number" required>
+        <input type="text" name="phone" id="phone" placeholder="Enter your phone number" required>
         <label for="password">Password</label>
-        <input type="password" id="password" placeholder="Enter your password" required>
+        <input type="password" name="pwd" id="password" placeholder="Enter your password" required>
         <label for="confirm-password">Confirm Password</label>
         <input type="password" id="confirm-password" placeholder="Confirm your password" required>
         <button type="submit">Sign Up</button>
