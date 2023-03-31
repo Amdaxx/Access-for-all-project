254b7d19def2b95128addff04cbaab77e0a88b05
Amdaxx <81655623+Amdaxx@users.noreply.github.com>
2023-03-31 13:16:02 +0100

    Update createAccount.php

--- Everbody-Welcome/userManagement/createAccount.php
+++ Everbody-Welcome/userManagement/createAccount.php
@@ -1,10 +1,11 @@
-<?php include '../public/publicHeader.php';
+<?php 
 include_once("../SQL/sql.php");
 
 if (isset($_POST['submit'])){
     createUser($_POST['email'], $_POST['pwd'], $_POST['name'], $_POST['phone'], $_POST['postcode'], $_POST['address']);
     }
     ?>
+<?php include '../public/publicHeader.php'; ?>
 <link rel="stylesheet" href="../css/userManagementStyle.css">
 <body>
     <div class="signup-container">
